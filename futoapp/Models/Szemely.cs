c755e40c63abdc6ed90bc5ec92524085d06c8e7b
FMáté <fejesm@kkszki.hu>
2026-01-15 10:59:03 +0100

    Osztályok létrehozása

---
+++ futoapp/Models/Szemely.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace futoapp.Models
+{
+    internal class Szemely
+    {
+        int magassag;
+        int testtomeg;
+        int nyugalmiPulzus;
+        int[] cel;
+
+        public int Magassag { get { return magassag; } set { magassag = value; } }
+        public int Testtomeg { get { return testtomeg; } set { testtomeg = value; } }
+        public int NyugalmiPulzus { get { return nyugalmiPulzus; } set { nyugalmiPulzus = value; } }
+        public int[] Cel { get { return cel; } set { cel = value; } }
+
+        public Szemely(int magassag, int testtomeg, int nyugalmiPulzus, int[] cel)
+        {
+            this.Magassag = magassag;
+            this.Testtomeg = testtomeg;
+            this.NyugalmiPulzus = nyugalmiPulzus;
+            this.Cel = cel;
+        }
+
+        public Szemely() { }
+
+
+    }
+}
